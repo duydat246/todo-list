41ceebc815f3ea3665ca5053c8face1a8086883f
duydat246 <duydat246@gmail.com>
2021-10-13 22:05:28 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-    return <div className="App">alo 1234</div>;
+    const [job, setJob] = useState("");
+    const [jobs, setJobs] = useState(() => {
+        const storageJobs = JSON.parse(localStorage.getItem("jobs"));
+        console.log(storageJobs);
+        return storageJobs ?? [];
+    });
+
+    const handleSubmit = () => {
+        setJobs((prev) => {
+            const newJobs = [...prev, job];
+            const jsonJobs = JSON.stringify(newJobs);
+            localStorage.setItem("jobs", jsonJobs);
+            return newJobs;
+        });
+        setJob("");
+    };
+    return (
+        <div style={{ padding: 32 }}>
+            <h1>MY TODO LISTS</h1>
+            <input value={job} onChange={(e) => setJob(e.target.value)} />
+            <button onClick={handleSubmit}>Add</button>
+            <ul>
+                {jobs.map((job, index) => (
+                    <li key={index}>{job}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
